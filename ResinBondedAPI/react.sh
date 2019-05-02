6e4846cdeaacd7d20558ba86d7f695cecbfbe87d
gilbertVirgo <gilbertjvirgo@gmail.com>
2019-05-02 13:16:58 +0100

    Added --force to svn

--- ResinBondedAPI/react.sh
+++ ResinBondedAPI/react.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /home/ubuntu/resin-bonded-api/ResinBondedAPI
-svn export https://github.com/zac-gil/rb-frontend/trunk/frontend/build
+svn export --force https://github.com/zac-gil/rb-frontend/trunk/frontend/build
